5ee3a8731764554ff23683e3d0d96d71bb6f595c
caravana34 <44247083+caravana34@users.noreply.github.com>
2021-08-31 12:24:14 -0500

    enlace formularios

---
+++ solicitudAdopcion.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi Mascota</title>
+    <!-- FONT AWESOME -->
+    <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">-->
+    <!--FONT OSWALD-->
+    <link rel = "preconnect" href = "https://fonts.gstatic.com">
+    <link href = "https://fonts.googleapis.com/css2? family = Oswald: wght @ 200; 300; 400 & display = swap "rel =" stylesheet ">
+    <link href = "https://fonts.googleapis.com/css2? family = Oswald: wght @ 200; 300; 400 & display = swap "rel =" stylesheet ">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+    <!-- CUSTOM CSS -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="menu-btn">
+        <i class="fas fa-bars"></i>
+
+    </div>
+
+
+    <div class="container5">
+        <nav class="nav-main"><a href="index.html" >
+         <img src="img/brand1-01.svg" alt="Logo Mimascota" class="nav-brand"></a> 
+           
+        </nav> 
+        
+        
+            <div>
+                <form class="regis">
+                    
+            
+                    Usuario     <br>           <input type="text" name="correoElectronico" size="30" maxlength="50" placeholder="Correo Electronico"> <br><br>
+                    Animal <br>    <input type="text" name="correoElectronico" size="30" maxlength="50" placeholder="E-mail"> <br><br>
+                    
+                    <input class="btn1" type="submit" name="btn" value="Iniciar">
+                 </form>
+
+
+        
+            </div>
+        
+    </div>
+
+    
+
+<footer class="footer">
+    <h3>Mi mascota copyright</h3>
+</footer>
+
+<!-- scroll reveal -->
+<script src="https://unpkg.com/scrollreveal"></script>
+
+<!-- custom js -->
+
+<script src="main.js"></script> 
+</body>
+</html>
