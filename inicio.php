f9b648a4e2e9cd72e42ce6cf8717e3ff96edb964
caravana34 <44247083+caravana34@users.noreply.github.com>
2021-09-01 16:26:34 -0500

    hojas de estilo para paginas

--- inicio.php
+++ inicio.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./inicio.css">
     <link rel = "preconnect" href = "https://fonts.gstatic.com">
     <link href = "https://fonts.googleapis.com/css2? family = Oswald: wght @ 200; 300; 400 & display = swap "rel =" stylesheet ">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
@@ -30,7 +30,7 @@
           
 
         Contraseña <input type="text" name="nombresUsuario" size="30" maxlength="50" placeholder="Contraseña"> <br><br>
-        <input class="btn2" type="submit" name="btn" value="Iniciar"></form></div>
+        <input class="btn1" type="submit" name="btn" value="Iniciar"></form></div>
         
     </div>
 
